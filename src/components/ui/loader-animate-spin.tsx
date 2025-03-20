95be93ec4874c47af4f8cd92404e21948b80bc03
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-20 14:53:18 +0000

    Add LoaderAnimateSpin component for displaying a loading animation with customizable context

---
+++ src/components/ui/loader-animate-spin.tsx
@@ -0,0 +1,14 @@
+import { Loader2Icon } from "lucide-react";
+
+interface LoaderProps {
+  context?: string;
+}
+
+export const LoaderAnimateSpin = ({ context = "Loading notes..." }: LoaderProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-1 mt-[20%]">
+      <Loader2Icon className="animate-spin" />
+      <p>{context}</p>
+    </div>
+  );
+};
