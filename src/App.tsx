998a9c0cbf94490e6bca74cae1035ffe933ac466
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-20 14:53:56 +0000

    Add routing for authentication and dashboard views with loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import { LoaderAnimateSpin } from "./components/ui/loader-animate-spin";
+import { ForgotPassword } from "./components/auth/ForgotPassword";
+import { CreateAccount } from "./components/auth/CreateAccount";
+import { Dashboard } from "./components/dashboard/Dashboard";
 import { ThemeProvider } from "./context/ThemeContext";
+import { useSession } from "./context/SessionContext";
+import { supabase } from "./supabase/supabaseClient";
 import { Header } from "./components/header/Header";
 import { Footer } from "./components/footer/Footer";
 import { Toaster } from "@/components/ui/sonner";
+import { Login } from "./components/auth/Login";
+import { useEffect, useState } from "react";
+import { ROUTES } from "./types/Routes";
 
 export const App = () => {
+  const [passwordRecovery, setPasswordRecovery] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const { session, setSession } = useSession();
+
+  useEffect(() => {
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, [supabase]);
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange(async (event) => {
+      if (event == "PASSWORD_RECOVERY") {
+        setPasswordRecovery(true);
+      }
+    });
+  }, [session]);
+
   return (
     <Router>
       <ThemeProvider
@@ -13,7 +45,20 @@ export const App = () => {
       >
         <div className="site-wrapper">
           <Header />
-          <div className="wrapper"></div>
+          <div className="wrapper">
+            <Routes>
+              {/* prettier-ignore */}
+              <Route path={ROUTES.HOME} element={session ? <Navigate replace to={ROUTES.DASHBOARD} /> : <Login />} />
+              {/* prettier-ignore */}
+              <Route path={ROUTES.CREATE_ACCOUNT} element={session ? <Navigate replace to={ROUTES.DASHBOARD} /> : <CreateAccount />} />
+              {/* prettier-ignore */}
+              <Route path={ROUTES.FORGOT_PASSWORD} element={session ? <Navigate replace to={ROUTES.DASHBOARD} /> : <ForgotPassword />} />
+              {/* prettier-ignore */}
+              <Route path={ROUTES.PASSWORD_RECOVERY} element={passwordRecovery ? <Navigate replace to={ROUTES.FORGOT_PASSWORD} /> : <Navigate replace to={ROUTES.HOME} />} />
+              {/* prettier-ignore */}
+              <Route path={ROUTES.DASHBOARD} element={session ? (loading ? <LoaderAnimateSpin /> : <Dashboard />) : <Navigate replace to={ROUTES.HOME} />} />
+            </Routes>
+          </div>
           <Footer />
           <Toaster richColors />
         </div>
