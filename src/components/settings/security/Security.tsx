a9a3aeb2083bcf49765127f1ff7c3be797276aee
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-21 11:11:56 +0000

    Add Security component for managing email and password changes

---
+++ src/components/settings/security/Security.tsx
@@ -0,0 +1,119 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import { KeyRoundIcon, MailIcon } from "lucide-react";
+import { useSession } from "@/context/SessionContext";
+import { ChangePassword } from "./ChangePassword";
+import { Button } from "@/components/ui/button";
+import { MENU_DATA } from "@/constants/menus";
+import { ChangeEmail } from "./ChangeEmail";
+import { Menu } from "@/types/Menu";
+import { useState } from "react";
+
+interface SecurityProps {
+  setActiveMenu: (menu: Menu) => void;
+}
+
+export const Security = ({ setActiveMenu }: SecurityProps) => {
+  const [showChangeEmail, setShowChangeEmail] = useState(false);
+  const [showChangePassword, setShowChangePassword] = useState(false);
+
+  const { session } = useSession();
+
+  return (
+    <main className="flex h-[480px] flex-1 flex-col overflow-hidden">
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <div className="flex items-center gap-2 px-4">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink>Settings</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Security</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <div className="flex flex-1 flex-col gap-4 overflow-y-auto p-4 pt-0">
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-col gap-2 w-fit">
+            <p className="scroll-m-20 text-l font-semibold tracking-tight">
+              Change your email address
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Update the email address associated with your account. This will be used for
+              account recovery and security notifications.
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Current email: <span className="font-semibold">{session?.user.email}</span>
+            </p>
+          </div>
+          <div className="flex items-center gap-2 m-3">
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => setShowChangeEmail(true)}
+            >
+              <MailIcon />
+            </Button>
+          </div>
+        </div>
+        <hr />
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-col gap-2 w-fit">
+            <p className="scroll-m-20 text-l font-semibold tracking-tight">
+              Change your password
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Update the password associated with your account. This will be used for
+              authentication and account security. Strongly recommend using a unique and
+              secure password.
+            </p>
+          </div>
+          <div className="flex items-center gap-2 m-3">
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => setShowChangePassword(true)}
+            >
+              <KeyRoundIcon />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {showChangeEmail && (
+        <ChangeEmail
+          open={showChangeEmail}
+          onOpenChange={() => setShowChangeEmail(!showChangeEmail)}
+        />
+      )}
+
+      {showChangePassword && (
+        <ChangePassword
+          open={showChangePassword}
+          onOpenChange={() => setShowChangePassword(!showChangePassword)}
+        />
+      )}
+
+      <div className="mt-auto grid gap-4 p-4 pt-0">
+        <Button
+          variant="outline"
+          className="md:hidden"
+          onClick={() => setActiveMenu(MENU_DATA[0].key as Menu)}
+        >
+          Back to Settings
+        </Button>
+      </div>
+    </main>
+  );
+};
