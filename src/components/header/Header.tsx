8eded5f934993d3e47e15796ab259ef96e414ce0
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-21 08:57:18 +0000

    Wrap Header component with AvatarProvider and integrate HeaderDropdownMenu for user session management

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,52 @@
+import { HeaderDropdownMenu } from "./HeaderDropdownMenu";
+import { AvatarProvider } from "@/context/AvatarContext";
+import { useSession } from "@/context/SessionContext";
 import { ModeToggle } from "../theme/ModeToggle";
 import { useNavigate } from "react-router-dom";
+import { CirclePlusIcon } from "lucide-react";
 import { ROUTES } from "@/types/Routes";
+import { Button } from "../ui/button";
 
 export const Header = () => {
+  const { session } = useSession();
+
   const navigate = useNavigate();
 
   return (
-    <header className="flex items-center justify-between p-4">
-      <div
-        className="flex gap-1.5 "
-        onClick={() => navigate(ROUTES.DASHBOARD)}
-        style={{ cursor: "pointer" }}
-      >
-        <img
-          src="/favicon.svg"
-          alt="Logo"
-          className="h-7"
-        />
-        <h3 className="scroll-m-20 text-2xl font-extrabold tracking-tight">Finances</h3>
-      </div>
-      <ModeToggle />
-    </header>
+    <AvatarProvider>
+      <header className="flex items-center justify-between p-3 pb-5">
+        <div
+          className="flex gap-1.5 "
+          onClick={() => navigate(ROUTES.DASHBOARD)}
+          style={{ cursor: "pointer" }}
+        >
+          <img
+            src="/favicon.svg"
+            alt="Logo"
+            className="h-7"
+          />
+          <h3 className="scroll-m-20 text-2xl font-extrabold tracking-tight">Finances</h3>
+        </div>
+        <div className="flex flex-col-reverse items-center justify-center gap-1.5 md:flex-row">
+          <div className="flex flex-col items-center gap-2 md:flex-row md:gap-2">
+            <div className="flex flex-row gap-2">
+              {session && (
+                <div className="flex flex-row items-center gap-2">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={() => navigate(ROUTES.DASHBOARD)}
+                  >
+                    <CirclePlusIcon />
+                  </Button>
+                </div>
+              )}
+              <ModeToggle />
+            </div>
+          </div>
+          {session && <HeaderDropdownMenu />}
+        </div>
+      </header>
+    </AvatarProvider>
   );
 };
